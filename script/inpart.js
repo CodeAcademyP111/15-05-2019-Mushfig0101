e87276bf8862a8e7c4d83e9073c56a5ea422cb4a
Mushfig M. Aghayev <MushfigMa@CODE.LOCAL>
2019-05-16 09:20:12 +0400

    16.05.2019

---
+++ script/inpart.js
@@ -0,0 +1,24 @@
+
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+
+    }
+    
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+    }
+
+
+
+   
+
+
+//  var button = document.querySelector("mySidenav");
+
+//  button.addEventListener("click", function(){
+//   li.style.width=250px;
+
+
+//  })
+
+ 
